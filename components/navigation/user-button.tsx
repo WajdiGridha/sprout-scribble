858c9a65bbfe2b3ff3852954d2e9eec4b17ce3d4
WajdiGridha <wajdigridha744@gmail.com>
2025-02-04 23:53:42 +0100

    Feat Creating Our Register Page

--- components/navigation/user-button.tsx
+++ components/navigation/user-button.tsx
@@ -1,14 +1,14 @@
-'use client'
-
+"use client"
 
 import { Session } from "next-auth"
 import { signOut } from "next-auth/react"
 
-export const UserButton = ({user}:Session)=>{
-    return(
+export const UserButton = ({ user }: Session) => {
+    return (
         <div>
             <h1>{user?.email}</h1>
-            <button onClick={()=> signOut()}>sign out</button>
+
+            <button onClick={() => signOut()}>sign out</button>
         </div>
     )
 }
--- server/schema.ts
+++ server/schema.ts
@@ -6,17 +6,24 @@ import {
   integer,
   boolean,
   pgEnum,
+  unique,
+  PgTable,
 } from "drizzle-orm/pg-core"
 import type { AdapterAccount } from "next-auth/adapters"
+import { createId } from "@paralleldrive/cuid2"
 
 export const RoleEnum = pgEnum("roles", ["user", "admin"])
 
 export const users = pgTable("user", {
-  id: text("id").notNull().primaryKey(),
+  id: text("id")
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => createId()),
   name: text("name"),
   email: text("email").notNull(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image"),
+  password: text("password"),
   twoFactorEnabled: boolean("twoFactorEnabled").default(false),
   role: RoleEnum("roles").default("user"),
 })
@@ -42,5 +49,49 @@ export const accounts = pgTable(
     compoundKey: primaryKey({
       columns: [account.provider, account.providerAccountId],
     }),
+  })
+)
+export const emailTokens = pgTable(
+  "email_tokens",
+  {
+    id: text("id")
+      .notNull()
+      .$defaultFn(() => createId()),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+    email: text("email").notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.id, vt.token] }),
+  })
+)
+
+export const passwordResetTokens = pgTable(
+  "password_reset_tokens",
+  {
+    id: text("id")
+      .notNull()
+      .$defaultFn(() => createId()),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+    email: text("email").notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.id, vt.token] }),
+  })
+)
+
+export const twoFactorTokens = pgTable(
+  "two_factor_tokens",
+  {
+    id: text("id")
+      .notNull()
+      .$defaultFn(() => createId()),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+    email: text("email").notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.id, vt.token] }),
   })
 )
