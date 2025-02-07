a1101d15e850537eb13b71fd09b11f916e7f987f
WajdiGridha <wajdigridha744@gmail.com>
2025-02-07 19:12:45 +0100

    Feat Settings Page

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,13 @@ const nextConfig = {
         remotePatterns: [
             { protocol: "https", hostname: "lh3.googleusercontent.com" },
             { protocol: "https", hostname: "avatars.githubusercontent.com" },
+            { protocol: "https", hostname: "utfs.io" },
         ],
     },
+    webpack: (config) => {
+        config.optimization.splitChunks = false;
+        return config;
+    }
 }
 
 export default nextConfig
